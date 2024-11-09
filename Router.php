087d97a3f5ca1b53994a0e65823d3f994e121f09
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2024-11-09 16:08:46 -0600

    Se asocian URLs y los metodos asociados a ellas

--- Router.php
+++ Router.php
@@ -3,8 +3,27 @@
 namespace MVC;
 
 class Router {
-    public function __construct()
-    {
-        echo "Creando el Router...";
+
+    public $rutasGET = [];
+    public $rutasPOST = [];
+
+    public function get($url, $fn) {
+        $this->rutasGET[$url] = $fn;
+    }
+    
+    public function comprobarRutas() {
+        $urlActual = $_SERVER['PATH_INFO'] ?? '/';
+        $metodo = $_SERVER['REQUEST_METHOD'];
+
+        if($metodo === 'GET'){
+            $fn = $this->rutasGET[$urlActual] ?? null;
+        }
+
+        if($fn) {
+            // La url existe y hay una funcion asociada
+            debuguear($fn);
+        } else {
+            call_user_func // call_user_func es una funcion que nos va a permitir llamar a una funcion cuando np sabemos como se llama esa funcion 
+        }
     }
 }
--- public/index.php
+++ public/index.php
@@ -5,3 +5,9 @@ require_once __DIR__ . '/../includes/app.php';
 use MVC\Router;
 
 $router = new Router();
+
+$router->get('/nosotros', 'funcion_nosotros');
+$router->get('/tienda_virtual', 'funcion_tienda');
+$router->get('/contacto', 'funcion_contacto');
+
+$router->comprobarRutas();
