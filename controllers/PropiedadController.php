90b1eb6c9932baafdd4f2bd931593305e01aa9de
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2024-11-13 21:37:06 -0600

    Se creael formulario para las nuevas propiedades

--- controllers/PropiedadController.php
+++ controllers/PropiedadController.php
@@ -3,6 +3,7 @@
 namespace Controllers;
 use MVC\Router;
 use Model\Propiedad;
+use Model\Vendedor;
 
 class PropiedadController {
     public static function index(Router $router) { // Agregamos Router para no crear una nueva instancia y seguir manteniendo la referencia pasando el objeto que viene de index.php
@@ -16,8 +17,15 @@ class PropiedadController {
         ]);
     }
 
-    public static function crear() { // Se agrega el static ya que de esta forma no requerimos de crear una nueva instancia
-        echo "propiedades/crear";
+    public static function crear(Router $router) { // Se agrega el static ya que de esta forma no requerimos de crear una nueva instancia
+
+        $propiedad = new Propiedad;
+        $vendedores = Vendedor::all();
+
+        $router->render('propiedades/crear', [
+            'propiedad' => $propiedad,
+            'vendedores' => $vendedores
+        ]);
     }
 
     public static function actualizar() {
---
+++ views/propiedades/crear.php
@@ -0,0 +1,8 @@
+<main class="contenedor seccion">
+        <h1>Crear</h1>
+        <form action="" class="formulario" methotd="POST" >
+            <?php include __DIR__ . '/formulario.php' ?>
+            
+            <input type="submit" value="Crear Popiedad" class="boton boton-verde">
+        </form>
+</main>
