a0046b3d582874fdedc2c7aa165921d8885a9fe6
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2025-02-24 01:54:07 -0600

    Se agregan las alertas en crear propiedad

--- controllers/PropiedadController.php
+++ controllers/PropiedadController.php
@@ -4,6 +4,7 @@ namespace Controllers;
 use MVC\Router;
 use Model\Propiedad;
 use Model\Vendedor;
+use Intervention\Image\ImageManagerStatic as Image;
 
 class PropiedadController {
     public static function index(Router $router) { // Agregamos Router para no crear una nueva instancia y seguir manteniendo la referencia pasando el objeto que viene de index.php
@@ -22,14 +23,49 @@ class PropiedadController {
         $propiedad = new Propiedad;
         $vendedores = Vendedor::all();
 
+        // Arreglo con mensajes de errores
+        $errores = Propiedad::getErrores();
+
          // Ejecutar el código después que el usuario envia el formulario 
         if($_SERVER['REQUEST_METHOD'] === 'POST') {
+            /** Crea una nueva instancia **/
+            $propiedad = new Propiedad($_POST['propiedad']);
 
+            /** SUBIDA DE ARCHIVOS **/
+
+            // Generar un nombre unico a la imagen
+            $nombreImagen = md5( uniqid( rand(), true ) ) . '.jpg';
+
+            // Setear la imagen
+            if($_FILES){
+                // Realiza un resize a la imagen con intervetion
+                $image = Image::make($_FILES['propiedad']['tmp_name']['imagen'])->fit(800,600);
+                $propiedad->setImagen($nombreImagen);
+            }
+
+            // Validar
+            $errores = $propiedad->validar();
+
+            // Revisar que el arreglo de errores este vacio
+            if(empty($errores)) { // Empty revisa que un arreglo este vacío
+
+                // Crear la carpeta
+                if(!is_dir(CARPETA_IMAGENES)){ //La función is_dir retorna si una carpeta existe o no existe
+                        mkdir(CARPETA_IMAGENES);
+                }
+
+                // Guarda la imagen en el servidor
+                $image->save(CARPETA_IMAGENES . $nombreImagen);
+
+                // Guarda en la base de datos
+                $propiedad->guardar();
+            }
         }
 
         $router->render('propiedades/crear', [
             'propiedad' => $propiedad,
-            'vendedores' => $vendedores
+            'vendedores' => $vendedores,
+            'errores' => $errores,
         ]);
     }
 
--- views/propiedades/crear.php
+++ views/propiedades/crear.php
@@ -1,5 +1,12 @@
 <main class="contenedor seccion">
     <h1>Crear</h1>
+
+    <?php foreach($errores as $error): ?>
+        <div class="alerta error">
+            <?php echo $error; ?>
+        </div>
+    <?php endforeach; ?>
+
     <form class="formulario" method="POST" >
         <?php include __DIR__ . '/formulario.php' ?>
 
