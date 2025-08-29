a43b13c13b7df9ea1264dd61abee51bb6ca28020
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2025-08-28 20:11:47 -0600

    Creando el controlador para vendedores

---
+++ controllers/VendedorController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Controllers;
+
+class VendedorController {
+    public static function crear(){
+        echo "Crear vendedor";
+    }
+    public static function actualizar(){
+        echo "Actualizar vendedor";
+    }
+    public static function Eliminar(){
+        echo "Eliminar vendedor";
+    }
+}
